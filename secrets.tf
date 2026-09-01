d8648f36857e27a8f260ff7cbedea0a366172fbd
meguito <doasilva2009@hotmail.com>
2026-09-01 11:07:43 -0300

    chore: add temporary default for mongo uri variable to unblock apply

--- secrets.tf
+++ secrets.tf
@@ -2,6 +2,10 @@ variable "api_messenger_mongo_uri" {
   type        = string
   description = "Connection string do MongoDB usado pelo api-messenger (spring.mongodb.uri)"
   sensitive   = true
+  # TODO: default temporário até o valor real ser definido via TF_VAR. Remover
+  # o default assim que o secret real for aplicado, pra não deixar Mongo mal
+  # configurado passar despercebido num apply futuro.
+  default = ""
 }
 
 resource "kubernetes_secret" "api_messenger" {
